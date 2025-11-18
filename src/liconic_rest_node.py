41914770b7e1ae319e7eafd18aa7dc5f402fb0b2
Ryan D. Lewis <ryan.lewis@anl.gov>
2025-11-17 21:33:41 -0600

    actions working, load and unload not tested

--- src/liconic_rest_node.py
+++ src/liconic_rest_node.py
@@ -138,11 +138,12 @@ class LiconicRestNode(RestNode):
 
     @action(
         name="set_target_temp",
-        # description="Set the target temperature of the incubator",
+        description="Set the target temperature of the incubator",
     )
     def set_target_temp(
-        self, temp: Annotated[float, "target temperature in celsius"]
-    ) -> None | ActionFailed:
+        self,
+        temp: Annotated[float, "target temperature in celsius"]
+    ) -> None:
         """Sets the target temperature of the incubator"""
 
         # Validate temperature argument with pydantic
@@ -181,7 +182,7 @@ class LiconicRestNode(RestNode):
     )
     def set_target_humidity(
         self, humidity: Annotated[float, "target humidity"]
-    ) -> ActionResult:
+    ) -> None:
         """Sets the target humidity of the incubator"""
 
         # Validate humidity argument with pydantic
@@ -212,205 +213,203 @@ class LiconicRestNode(RestNode):
             return ActionFailed(errors=str(err))
         else:
             self.logger.log("Target humidity set successfully.")
-            # return ActionSucceeded()
             return None
 
-    # @action(
-    #     name="begin_shake",
-    #     description="Begin shaking the incubator at the specified speed",
-    # )
-    # def begin_shake(
-    #     self,
-    #     shaker_id: Annotated[
-    #         Optional[int],
-    #         "shaker id (shaker 1 = stacks 1 and 2, shaker 2 = stacks 3 and 4 , or None for both shakers)",
-    #     ] = None,
-    #     shaker_speed: Annotated[
-    #         int, "shaker speed (1-50 valid, default 20 = 200rpm)"
-    #     ] = 20,
-    # ) -> ActionResult:
-    #     """Activate the shaker in the LiCONiC incubator at the specified shaker_speed'"""
+    @action(
+        name="begin_shake",
+        description="Begin shaking the incubator at the specified speed",
+    )
+    def begin_shake(
+        self,
+        shaker_id: Annotated[
+            Optional[int],
+            "shaker id (shaker 1 = stacks 1 and 2, shaker 2 = stacks 3 and 4 , or None for both shakers)",
+        ] = None,
+        shaker_speed: Annotated[
+            int, "shaker speed (1-50 valid, default 20 = 200rpm)"
+        ] = 20,
+    ) -> None:
+        """Activate the shaker in the LiCONiC incubator at the specified shaker_speed'"""
 
-    #     # Validate arguments with pydantic model
-    #     try:
-    #         BeginShakeModel(
-    #             shaker_id=shaker_id,
-    #             shaker_speed=shaker_speed,
-    #         )
-    #     except Exception as err:
-    #         # Don't put device into error state if argument validation fails, just fail action
-    #         self.logger.log_error(f"Error validating begin_shake arguments: {err}")
-    #         return ActionFailed(errors=str(err))
+        # Validate arguments with pydantic model
+        try:
+            BeginShakeModel(
+                shaker_id=shaker_id,
+                shaker_speed=shaker_speed,
+            )
+        except Exception as err:
+            # Don't put device into error state if argument validation fails, just fail action
+            self.logger.log_error(f"Error validating begin_shake arguments: {err}")
+            return ActionFailed(errors=str(err))
 
-    #     # Activate shaker(s)
-    #     if shaker_id and str(shaker_id).lower() != "none":
-    #         self.liconic_interface.activate_shaker(
-    #             shaker_id=int(shaker_id), speed=shaker_speed
-    #         )
-    #         time.sleep(2)
-    #     elif shaker_id is None or str(shaker_id).lower() == "none":
-    #         # activate both shakers
-    #         self.liconic_interface.activate_shaker(shaker_id=1, speed=shaker_speed)
-    #         time.sleep(2)
-    #         self.liconic_interface.activate_shaker(shaker_id=2, speed=shaker_speed)
-    #         time.sleep(2)
-    #     else:
-    #         self.logger.log_error("Error starting shaker, invalid shaker id.")
-    #         return ActionFailed(errors="Error starting shaker, invalid shaker id.")
-    #     return ActionSucceeded()
+        # Activate shaker(s)
+        if shaker_id and str(shaker_id).lower() != "none":
+            self.liconic_interface.activate_shaker(
+                shaker_id=int(shaker_id), speed=shaker_speed
+            )
+            time.sleep(2)
+        elif shaker_id is None or str(shaker_id).lower() == "none":
+            # activate both shakers
+            self.liconic_interface.activate_shaker(shaker_id=1, speed=shaker_speed)
+            time.sleep(2)
+            self.liconic_interface.activate_shaker(shaker_id=2, speed=shaker_speed)
+            time.sleep(2)
+        else:
+            self.logger.log_error("Error starting shaker, invalid shaker id.")
+            return ActionFailed(errors="Error starting shaker, invalid shaker id.")
+        return None
 
-    # @action(name="end_shake", description="Stop shaking the incubator")
-    # def end_shake(
-    #     self,
-    #     shaker_id: Annotated[
-    #         Optional[int],
-    #         "shaker id (shaker 1 = stacks 1 and 2, shaker 2 = stacks 3 and 4 , or None for both shakers)",
-    #     ] = None,
-    # ) -> ActionResult:
-    #     """Stop the shaker in the LiCONiC incubator"""
+    @action(name="end_shake", description="Stop shaking the incubator")
+    def end_shake(
+        self,
+        shaker_id: Annotated[
+            Optional[int],
+            "shaker id (shaker 1 = stacks 1 and 2, shaker 2 = stacks 3 and 4 , or None for both shakers)",
+        ] = None,
+    ) -> None:
+        """Stop the shaker in the LiCONiC incubator"""
 
-    #     # Validate arguments with pydantic model
-    #     try:
-    #         EndShakeModel(
-    #             shaker_id=shaker_id,
-    #         )
-    #     except Exception as err:
-    #         # Fail action, don't put device into error state
-    #         self.logger.log_error(f"Error validating end_shake arguments: {err}")
-    #         return ActionFailed(errors=str(err))
+        # Validate arguments with pydantic model
+        try:
+            EndShakeModel(
+                shaker_id=shaker_id,
+            )
+        except Exception as err:
+            # Fail action, don't put device into error state
+            self.logger.log_error(f"Error validating end_shake arguments: {err}")
+            return ActionFailed(errors=str(err))
 
-    #     # Deactivate shaker(s)
-    #     if shaker_id and str(shaker_id).lower() != "none":
-    #         # deactivate specified shaker
-    #         self.liconic_interface.deactivate_shaker(shaker_id=int(shaker_id))
-    #         time.sleep(2)
-    #     elif shaker_id is None or str(shaker_id).lower() == "none":
-    #         # deactivate both shakers
-    #         self.liconic_interface.deactivate_shaker(shaker_id=1)
-    #         time.sleep(2)
-    #         self.liconic_interface.deactivate_shaker(shaker_id=2)
-    #         time.sleep(2)
-    #     else:
-    #         self.logger.log_error("Error stopping shaker, invalid shaker id.")
-    #         return ActionFailed(errors="Error stopping shaker, invalid shaker id.")
-    #     return ActionSucceeded()
+        # Deactivate shaker(s)
+        if shaker_id and str(shaker_id).lower() != "none":
+            # deactivate specified shaker
+            self.liconic_interface.deactivate_shaker(shaker_id=int(shaker_id))
+            time.sleep(2)
+        elif shaker_id is None or str(shaker_id).lower() == "none":
+            # deactivate both shakers
+            self.liconic_interface.deactivate_shaker(shaker_id=1)
+            time.sleep(2)
+            self.liconic_interface.deactivate_shaker(shaker_id=2)
+            time.sleep(2)
+        else:
+            self.logger.log_error("Error stopping shaker, invalid shaker id.")
+            return ActionFailed(errors="Error stopping shaker, invalid shaker id.")
+        return None
 
-    # @action(name="load_plate", description="Load a plate into the incubator")
-    # def load_plate(
-    #     self,
-    #     plate_type: Annotated[
-    #         str, "plate type (flat_bottom_96well, deep_96well, etc.)"
-    #     ],
-    #     plate_id: Annotated[Optional[str], "plate id"] = None,
-    #     stack: Annotated[
-    #         Optional[int], "stack number (1-4), must also specify slot"
-    #     ] = None,
-    #     slot: Annotated[
-    #         Optional[int],
-    #         "slot number (1-22 for stacks 1 and 2, 1-10 for stacks 3 and 4, must also specify stack)",
-    #     ] = None,
-    # ) -> ActionResult:
-    #     """Load a plate into the incubator"""
+    @action(name="load_plate", description="Load a plate into the incubator")
+    def load_plate(
+        self,
+        plate_type: Annotated[
+            str, "plate type (flat_bottom_96well, deep_96well, etc.)"
+        ],
+        plate_id: Annotated[Optional[str], "plate id"] = None,
+        stack: Annotated[
+            Optional[int], "stack number (1-4), must also specify slot"
+        ] = None,
+        slot: Annotated[
+            Optional[int],
+            "slot number (1-22 for stacks 1 and 2, 1-10 for stacks 3 and 4, must also specify stack)",
+        ] = None,
+    ) -> None:
+        """Load a plate into the incubator"""
 
-    #     # Validate arguments with pydantic model
-    #     try:
-    #         self.logger.info(
-    #             f"Resource tracker before LoadPlateModel: {self.module_resources}, type {type(self.module_resources)}"
-    #         )
-    #         LoadPlateModel(
-    #             plate_type=plate_type,
-    #             plate_id=plate_id,
-    #             stack=stack,
-    #             slot=slot,
-    #             resource_tracker=self.module_resources,
-    #         )
-    #     except Exception as err:
-    #         # Don't put device into error state if argument validation fails, just fail action
-    #         self.logger.log_error(f"Error validating load_plate arguments: {err}")
-    #         return ActionFailed(errors=str(err))
+        # Validate arguments with pydantic model
+        try:
+            self.logger.info(
+                f"Resource tracker before LoadPlateModel: {self.module_resources}, type {type(self.module_resources)}"
+            )
+            LoadPlateModel(
+                plate_type=plate_type,
+                plate_id=plate_id,
+                stack=stack,
+                slot=slot,
+                resource_tracker=self.module_resources,
+            )
+        except Exception as err:
+            # Don't put device into error state if argument validation fails, just fail action
+            self.logger.log_error(f"Error validating load_plate arguments: {err}")
+            return ActionFailed(errors=str(err))
 
-    #     # Find next free slot if none specified
-    #     if stack is None and slot is None:
-    #         stack, slot = self.module_resources.get_next_free_slot(
-    #             plate_type=plate_type
-    #         )
+        # Find next free slot if none specified
+        if stack is None and slot is None:
+            stack, slot = self.module_resources.get_next_free_slot(
+                plate_type=plate_type
+            )
 
-    #     # 5. Check if there's a plate in the transfer station
-    #     if self.liconic_interface.read_transfer_station_detector() == 0:
-    #         self.logger.log_error(
-    #             "Load_plate command cannot be completed, no plate in transfer station."
-    #         )
-    #         return ActionFailed(
-    #             "Load_plate command cannot be completed, no plate in transfer station"
-    #         )
+        # 5. Check if there's a plate in the transfer station
+        if self.liconic_interface.read_transfer_station_detector() == 0:
+            self.logger.log_error(
+                "Load_plate command cannot be completed, no plate in transfer station."
+            )
+            return ActionFailed(
+                "Load_plate command cannot be completed, no plate in transfer station"
+            )
 
-    #     # Load the plate
-    #     self.liconic_interface.load_plate(stack=stack, slot=slot)
-    #     while self.liconic_interface.is_busy:
-    #         time.sleep(1)
-    #     self.module_resources.add_plate(
-    #         plate_id=plate_id,
-    #         stack=stack,
-    #         slot=slot,
-    #         plate_type=plate_type,
-    #     )
-    #     return ActionSucceeded(
-    #         data={"message": f"Plate loaded into LiCONiC stack {stack}, slot {slot}"}
-    #     )
+        # Load the plate
+        self.liconic_interface.load_plate(stack=stack, slot=slot)
+        while self.liconic_interface.is_busy:
+            time.sleep(1)
+        self.module_resources.add_plate(
+            plate_id=plate_id,
+            stack=stack,
+            slot=slot,
+            plate_type=plate_type,
+        )
+        self.logger.log_info(
+            f"Plate loaded into LiCONiC stack {stack}, slot {slot}"
+        )
+        return None
 
-    # @action(name="unload_plate", description="Unload a plate from the incubator")
-    # def unload_plate(
-    #     self,
-    #     plate_id: Annotated[Optional[str], "plate id"] = None,
-    #     stack: Annotated[Optional[int], "stacker number (1-4)"] = None,
-    #     slot: Annotated[
-    #         Optional[int],
-    #         "slot number (1-22 for stacks 1 and 2, 1-10 for stacks 3 and 4)",
-    #     ] = None,
-    # ) -> ActionResult:
-    #     """Unload a plate from the incubator"""
+    @action(name="unload_plate", description="Unload a plate from the incubator")
+    def unload_plate(
+        self,
+        plate_id: Annotated[Optional[str], "plate id"] = None,
+        stack: Annotated[Optional[int], "stacker number (1-4)"] = None,
+        slot: Annotated[
+            Optional[int],
+            "slot number (1-22 for stacks 1 and 2, 1-10 for stacks 3 and 4)",
+        ] = None,
+    ) -> None:
+        """Unload a plate from the incubator"""
 
-    #     # Validate arguments with pydantic model
-    #     self.logger.info(
-    #         f"Unload plate arguments before validation: plate_id={plate_id}, stack={stack}, slot={slot}"
-    #     )
-    #     try:
-    #         self.logger.info(
-    #             f"Resource tracker before LoadPlateModel: {self.module_resources}, type {type(self.module_resources)}"
-    #         )
-    #         model = UnloadPlateModel(
-    #             plate_id=plate_id,
-    #             stack=stack,
-    #             slot=slot,
-    #             resource_tracker=self.module_resources,
-    #         )
-    #         # Extract validated values
-    #         plate_id = model.plate_id
-    #         stack = model.stack
-    #         slot = model.slot
-    #     except Exception as err:
-    #         # Don't put device into error state if argument validation fails, just fail action
-    #         self.logger.log_error(f"Error validating unload_plate arguments: {err}")
-    #         return ActionFailed(errors=str(err))
+        # Validate arguments with pydantic model
+        self.logger.info(
+            f"Unload plate arguments before validation: plate_id={plate_id}, stack={stack}, slot={slot}"
+        )
+        try:
+            self.logger.info(
+                f"Resource tracker before LoadPlateModel: {self.module_resources}, type {type(self.module_resources)}"
+            )
+            model = UnloadPlateModel(
+                plate_id=plate_id,
+                stack=stack,
+                slot=slot,
+                resource_tracker=self.module_resources,
+            )
+            # Extract validated values
+            plate_id = model.plate_id
+            stack = model.stack
+            slot = model.slot
+        except Exception as err:
+            # Don't put device into error state if argument validation fails, just fail action
+            self.logger.log_error(f"Error validating unload_plate arguments: {err}")
+            return ActionFailed(errors=str(err))
 
-    #     # Ensure transfer station is clear
-    #     if self.liconic_interface.read_transfer_station_detector() == 1:
-    #         self.logger.log_error(
-    #             "Transfer station occupied, please clear it before unloading."
-    #         )
-    #         return ActionFailed(
-    #             errors="Transfer station occupied, please clear it before unloading."
-    #         )
+        # Ensure transfer station is clear
+        if self.liconic_interface.read_transfer_station_detector() == 1:
+            self.logger.log_error(
+                "Transfer station occupied, please clear it before unloading."
+            )
+            return ActionFailed(
+                errors="Transfer station occupied, please clear it before unloading."
+            )
 
-    #     # Unload the plate
-    #     self.liconic_interface.unload_plate(stack=stack, slot=slot)
-    #     while self.liconic_interface.is_busy:
-    #         time.sleep(1)
-    #     self.module_resources.remove_plate(stack=stack, slot=slot)
-    #     self.logger.info("Plate unloaded successfully.")
-    #     return ActionSucceeded(
-    #         data={"message": f"Plate unloaded from LiCONiC stack {stack}, slot {slot}"}
-    #     )
+        # Unload the plate
+        self.liconic_interface.unload_plate(stack=stack, slot=slot)
+        while self.liconic_interface.is_busy:
+            time.sleep(1)
+        self.module_resources.remove_plate(stack=stack, slot=slot)
+        self.logger.info(f"Plate unloaded from LiCONiC stack {stack}, slot {slot}")
+        return None
 
 
 if __name__ == "__main__":
